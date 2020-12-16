f874b0a2148e491c9ef0dcce6e24da76156812ba
quang <quangtm199@gmail.com>
2020-12-16 19:08:24 +0700

    m

---
+++ LAB04/AllTests.java
@@ -0,0 +1,11 @@
+package controller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ValidateAddressTest.class,ValidateNameTest.class,ValidateNameTest.class})
+public class AllTests {
+
+}
